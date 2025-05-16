3e17a928519a73325ef74f8a9235bf50072f3bed
Icosa2050 <Icosa2050@users.noreply.github.com>
2025-05-16 00:21:21 +0000

    Deploying to gh-pages from @ Icosa2050/ui5-webcomponents@27c8bb7ba6e9d153d3da2ad2d976e7ab4cc13714 🚀

--- nightly/assets/test/pages/Icon.html-7wiCqpM5.js
+++
@@ -1,1 +0,0 @@
-import"../../bundle.esm-uvMSZKwq.js";(async()=>{var a=await window["sap-ui-webcomponents-bundle"].getIconNames();a.forEach(e=>{var n=document.createElement("ui5-icon");n.name=e,allIcons.appendChild(n)})})();
--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.3",
     isNext: false,
-    buildTime: 1747268166,
+    buildTime: 1747354653,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
