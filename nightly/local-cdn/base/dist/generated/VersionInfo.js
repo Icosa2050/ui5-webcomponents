f647f173bf787812cee8c2f8e7783e2255ece55f
Icosa2050 <Icosa2050@users.noreply.github.com>
2025-07-26 00:22:04 +0000

    Deploying to gh-pages from @ Icosa2050/ui5-webcomponents@27c8bb7ba6e9d153d3da2ad2d976e7ab4cc13714 🚀

--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.3",
     isNext: false,
-    buildTime: 1753402746,
+    buildTime: 1753489098,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
