f5c0f80d08d8cddb2fb9907aa4a0ff629587e76a
Icosa2050 <Icosa2050@users.noreply.github.com>
2026-03-06 00:53:38 +0000

    Deploying to gh-pages from @ Icosa2050/ui5-webcomponents@89ccfb8b227a92ebb7c831b13f8e26e0b47177d7 🚀

--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.2",
     isNext: false,
-    buildTime: 1772671591,
+    buildTime: 1772758143,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
--- nightly/local-cdn/main/dist/assets/test/pages/Icon.html-DvfbkjRC.js
+++
@@ -1,1 +0,0 @@
-import"../../bundle.esm-qJIEZb5m.js";(async()=>{var a=await window["sap-ui-webcomponents-bundle"].getIconNames();a.forEach(e=>{var n=document.createElement("ui5-icon");n.name=e,allIcons.appendChild(n)})})();
