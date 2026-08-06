4bb78be4da422e23bd960a3e80437694600096b7
Icosa2050 <Icosa2050@users.noreply.github.com>
2026-08-06 00:47:31 +0000

    Deploying to gh-pages from @ Icosa2050/ui5-webcomponents@ffd674c78573ddc315c0f6d7a92068aeb120b660 🚀

--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.3",
     isNext: false,
-    buildTime: 1785890737,
+    buildTime: 1785976962,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
--- nightly/local-cdn/main/dist/assets/ListItemStandardExpandableTextTemplate-C75GwgaL.js
+++
@@ -1,1 +0,0 @@
-import{e as x,E as m}from"./bundle.esm-BPjmUn4q.js";function p(a){const{className:t,text:e,maxCharacters:r,part:s}=a;return x(m,{part:s,class:t,text:e,maxCharacters:r})}export{p as default};
