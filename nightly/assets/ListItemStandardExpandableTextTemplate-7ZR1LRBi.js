d301abafaf6293fc15b54f75a0171b4174ccfc7b
Icosa2050 <Icosa2050@users.noreply.github.com>
2026-08-23 00:28:44 +0000

    Deploying to gh-pages from @ Icosa2050/ui5-webcomponents@ffd674c78573ddc315c0f6d7a92068aeb120b660 🚀

---
+++ nightly/assets/ListItemStandardExpandableTextTemplate-7ZR1LRBi.js
@@ -0,0 +1,1 @@
+import{e as x,E as m}from"./bundle.esm-Z8x6s-KL.js";function p(a){const{className:t,text:e,maxCharacters:r,part:s}=a;return x(m,{part:s,class:t,text:e,maxCharacters:r})}export{p as default};
